e124752d09c52983b4e81dcb58bfd9fb1be43276
Ali <alimohammadnia127@gmail.com>
2025-10-19 19:37:13 +0330

    add *  to ALLOWED_HOSTS

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'django-insecure-+0-ufe6nuvbf8@fxfdr3m+f_ur@h9emihufes(o@3ezk*r__y+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
